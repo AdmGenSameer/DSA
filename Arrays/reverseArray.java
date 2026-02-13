66efd8134af5bd663ecd84c948d8886b29b3a97c
Sameer Chaudhary <154604600+AdmGenSameer@users.noreply.github.com>
2026-02-13 08:41:48 +0530

    pp

---
+++ Arrays/reverseArray.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+class reverseArray{
+
+    public static void reverse(int nums[]){
+
+        int first = 0;
+        int last= nums.length - 1;
+        
+        for(int i=0; i<nums.length/2; i++ ){
+
+            first = nums[i];
+            
+
+        }
+
+    };
+
+    public static void main(String[] args) {
+        int nums[] = {1,5,3,7,9}
+    }
+}
